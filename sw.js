43c141a39ea82005be8485e46cd4cfa0aa2a1375
AppADayCreator <appadaycreator@gmail.com>
2026-06-13 03:26:47 +0900

    Apply: design update from auto_design step6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'food-allergy-checker-v1.2.1';
+const CACHE_NAME = 'food-allergy-checker-v1.2.2';
 const ASSETS = [
   '/food-allergy-checker/',
   '/food-allergy-checker/index.html',
